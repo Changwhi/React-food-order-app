d1cbc8d987d6804375e69c1a29e76c51f51fb714
Changwhi <whinada@gmail.com>
2023-07-24 13:45:37 -0700

    Feat: creat custom hook for http request

---
+++ src/hook/request.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const useRequest = (requestInfo, option) => {
+    const [currentState, setCurrentState] = useState({
+        isLoading: false,
+        error: '',
+    });
+
+    const sendRequest = async () => {
+        setCurrentState({
+            ...currentState,
+            isLoading: true,
+            error: '',
+        });
+
+        try {
+            const response = await fetch(
+                requestInfo.url,
+                {
+                    headers: requestInfo.headers ? requestInfo.headers : {},
+                    body: requestInfo.body ? requestInfo.body : null,
+                    method: requestInfo.method ? requestInfo.method : "GET",
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error('Request failed!');
+            }
+            const data = response.json();
+            option(data);
+        }
+        catch (err) {
+            setCurrentState({
+                ...currentState,
+                error: err.message || "Something went wrong!"
+            });
+        }
+        setCurrentState({
+            ...currentState,
+            isLoading: false,
+        });
+    };
+
+    return {
+        sendRequest,
+        error: currentState.error,
+        isLoading: currentState.isLoading,
+    }
+};
+
+export default useRequest;
